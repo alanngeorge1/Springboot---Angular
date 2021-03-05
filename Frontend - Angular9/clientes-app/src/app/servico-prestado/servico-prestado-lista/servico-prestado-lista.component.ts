8fa7efde16eed5a87ad9bac590baca1cb637c086
alanngeorge1 <alanngeorge1@gmail.com>
2021-03-04 23:44:49 -0300

    Finalizando filtro de pesquisa

--- Frontend - Angular9/clientes-app/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ Frontend - Angular9/clientes-app/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
+import { ServicoPrestadoService } from '../../servico-prestado.service'
+
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -9,16 +12,29 @@ export class ServicoPrestadoListaComponent implements OnInit {
 
   nome: string;
   mes: number;
-  meses: number [];
-  constructor() { 
-    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12]
+  meses: number[];
+  lista: ServicoPrestadoBusca[];
+  message: string;
+
+  constructor(
+    private service: ServicoPrestadoService
+  ) { 
+    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12];
   }
 
   ngOnInit(): void {
   }
 
   consultar(){
-    console.log(this.nome, this.mes)
+    this.service
+      .buscar(this.nome, this.mes)
+      .subscribe(response => {
+        this.lista = response;
+        if( this.lista.length <= 0 ){
+          this.message = "Nenhum Registro encontrado.";
+        }else{
+          this.message = null;
+        }
+      });
   }
-
 }
