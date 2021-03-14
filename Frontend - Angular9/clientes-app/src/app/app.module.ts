5d4d605ef51362e2aeb55b5b0f6875144a0dbda7
alanngeorge1 <alanngeorge1@gmail.com>
2021-03-14 02:05:16 -0300

    Tela de login

--- Frontend - Angular9/clientes-app/src/app/app.module.ts
+++ Frontend - Angular9/clientes-app/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
-
-
+import { FormsModule } from '@angular/forms'
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,21 +13,27 @@ import { ClientesService}       from './clientes.service';
 import { ServicoPrestadoService} from './servico-prestado.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { from } from 'rxjs';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
+    LayoutComponent,
     
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     TemplateModule,
     ClientesModule,
     ServicoPrestadoModule,
+    
   ],
   
   providers: [
--- Frontend - Angular9/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ Frontend - Angular9/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -43,7 +43,7 @@ export class ClientesFormComponent implements OnInit {
     }
   
   voltarParaLista(){
-    this.router.navigate(['/clientes-lista'])
+    this.router.navigate(['/clientes/lista'])
   }
 
   onSubmit(){
