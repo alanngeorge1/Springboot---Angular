ce13a19368659fd2f1816aed5e383517d1cc74e1
alanngeorge1 <alanngeorge1@gmail.com>
2021-03-23 10:57:21 -0300

    Oauth2

--- Backend - Spring/src/main/java/com/alanngeorge1/AngularJava/model/repository/UsuarioRepository.java
+++ Backend - Spring/src/main/java/com/alanngeorge1/AngularJava/model/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ package com.alanngeorge1.AngularJava.model.repository;
 import com.alanngeorge1.AngularJava.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository  extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
 
 }
